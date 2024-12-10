909fe2f1219ffb6ff2672512d1bc4d990c0fe08e
RPhilbo <77111162+RPhilbo@users.noreply.github.com>
2024-12-10 21:46:38 +0100

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -10,4 +10,4 @@ sudo apt install libnova-dev libcfitsio-dev libusb-1.0-0-dev zlib1g-dev libgsl-d
         wcslib-dev libqt5websockets5-dev xplanet xplanet-images qt5keychain-dev libsecret-1-dev breeze-icon-theme qml-module-qtquick-controls \
         pkg-config libev-dev libqt5datavisualization5-dev libzmq3-dev
 
-[ "$1" == "phd2" ] && sudo apt install libwxgtk3.2-dev
+[ "$1" == "phd2" ] && sudo apt install libwxgtk3.2-dev wx-common wx3.2-i18n libindi-dev libx11-dev libopencv-dev libgtest-dev
